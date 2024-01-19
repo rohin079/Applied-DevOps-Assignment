e2b693a1c2f39cd36e62865c64b46162a143308f
ArpitGoyal8811 <500094790@stu.upes.ac.in>
2024-01-19 09:25:50 +0530

    first push

---
+++ sample.py
@@ -0,0 +1,10 @@
+# main.py
+
+def main_function():
+    print("This is the main function.")
+
+def feature_function():
+    print("This is the feature function.")
+
+if __name__ == "__main__":
+    main_function()
