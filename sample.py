2fd7df2ccd80f62a1d66d94277744d7233e1088e
ArpitGoyal8811 <500094790@stu.upes.ac.in>
2024-01-19 10:33:31 +0530

    Made changes in branch2

--- sample.py
+++ sample.py
@@ -4,7 +4,8 @@ def main_function():
     print("This is the main function.")
 
 def feature_function():
-    print("This is the feature function.")
+    print("This is the second feature function.")
+    print("Second changes done in branch2")
 
 if __name__ == "__main__":
     main_function()
